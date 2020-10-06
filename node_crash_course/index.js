b1e4b94783ee1204ef03e246b20fae175b695567
Joshua MacSween <joshmacsween@gmail.com>
2020-10-06 17:10:16 -0600

    basic working server

--- node_crash_course/index.js
+++ node_crash_course/index.js
@@ -1,5 +1,31 @@
-const Person = require('./person')
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
 
-const person1 = new Person('John Doe', 30)
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile(
+      path.join(__dirname, 'public', 'index.html'),
+      (err, content) => {
+        if (err) throw error
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.end(content)
+      }
+    )
+  }
 
-person1.greeting()
+  if (req.url === '/about') {
+    fs.readFile(
+      path.join(__dirname, 'public', 'about.html'),
+      (err, content) => {
+        if (err) throw error
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.end(content)
+      }
+    )
+  }
+})
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
