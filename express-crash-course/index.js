842cd4e8a84585411c5eca6434ac64264ad39204
Joshua MacSween <joshmacsween@gmail.com>
2020-10-07 18:23:30 -0600

    using postman

--- express-crash-course/index.js
+++ express-crash-course/index.js
@@ -6,7 +6,12 @@ const logger = require('./middleware/logger')
 const app = express()
 
 //Use middleware
-app.use(logger)
+// app.use(logger)
+
+//Get single member
+app.get('/api/members/:id', (req, res) => {
+  res.json(members.filter(member => member.id === parseInt(req.params.id)))
+})
 
 //Gets all members
 app.get('/api/members', (req, res) => res.json(members))
