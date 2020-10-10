e87dc156b270732f1ef43dc3ad1031b3abeae7f8
Joshua MacSween <joshmacsween@gmail.com>
2020-10-10 17:12:36 -0600

    connected to atlas

---
+++ mern-exercise-tracker/backend/models/exercise.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose'); // Erase if already required
+
+const Schema = mongoose.Schema
+// Declare the Schema of the Mongo model
+const exerciseSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+}, {
+  timestamps: true,
+});
+
+const Exercise = mongoose.model('Exercise', exerciseSchema)
+
+//Export the model
+module.exports = Exercise
---
+++ mern-exercise-tracker/backend/models/user.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema
+
+// Declare the Schema of the Mongo model
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+}, {
+  timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema)
+
+//Export the model
+module.exports = User
