0f1a2865cbf112d4c4214b18b090adaf04ea6d30
Joshua MacSween <joshmacsween@gmail.com>
2020-10-12 11:34:45 -0600

    CRUD exercises

--- mern-exercise-tracker/backend/models/exercise.model.js
+++ mern-exercise-tracker/backend/models/exercise.model.js
@@ -6,7 +6,7 @@ const exerciseSchema = new Schema({
     username: {
         type: String,
         required: true,
-        unique: true,
+        // unique: true,
     },
     description: {
         type: String,
@@ -21,7 +21,7 @@ const exerciseSchema = new Schema({
         required: true,
     },
 }, {
-  timestamps: true,
+    timestamps: true,
 });
 
 const Exercise = mongoose.model('Exercise', exerciseSchema)
